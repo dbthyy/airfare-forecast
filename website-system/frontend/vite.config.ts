3bdb75f0181a8080900f7a2849117dc2e738f5db
dbt <23521563@gm.uit.edu.vn>
2026-06-09 19:50:02 +0700

    push code

--- website-system/frontend/vite.config.ts
+++ website-system/frontend/vite.config.ts
@@ -3,6 +3,7 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
+// Thêm github repo
 
 function figmaAssetResolver() {
   return {
@@ -17,6 +18,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: 'airfare-forecast',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
